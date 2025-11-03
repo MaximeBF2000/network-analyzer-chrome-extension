fa7a11d48c6bc08d4054e8da6d5f31b8526d875c
Maxime FERRET <maxime.bounaas@gmail.com>
2025-11-03 21:17:41 +0100

    refacto(): remove contentScript (only used for logging request)

--- content.js
+++
@@ -1,7 +0,0 @@
-// Listen for messages from background script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'NETWORK_REQUEST') {
-    console.log('Network Request:', message.data)
-  }
-  return true
-})
